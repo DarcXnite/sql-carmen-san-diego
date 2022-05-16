45491b82d5c0784f1924b611ce1ac7da1a2f6070
DarcXnite <anhri89@yahoo.com>
2022-05-16 16:25:23 -0500

    finished lab shes in santa monica

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,19 +1,29 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
+select * from countries where region='Southern Europe';
 
+VAT  | Holy See (Vatican Cities State) | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                |       9.00 |            | Santa Sede/Citt� del Vaticano | Independent Church State      | Johannes Paavali II  |    3538 | VA
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
+countrycode VAT
+select * from countrylanguages where countrycode='VAT';
+ VAT         | Italian  | t          |          0
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+isofficial = t
+language = Italian
+select * from countrylanguages where language='Italian';
+SMR         | Italian  | t          |        100
+
+SMR  | San Marino | Europe    | Southern Europe |          61 |       885 |      27000 | 
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
@@ -21,18 +31,26 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+select * from cities where countrycode='SMR';
+3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+
+
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-
+select * from countries where region='South America';
+SUR  | Suriname         | South America | South America |       163265 |      1975 |     417000 |           71.4 |    870.00 |    
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
+capital 3243
+select * from cities where id=3243;
+3243 | Paramaribo | SUR         | Paramaribo |     11200
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
@@ -54,3 +72,6 @@
 
 
 -- She's in ____________________________!
+
+select * from cities where population=91084;
+ 4060 | Santa Monica | USA         | California |      91084
